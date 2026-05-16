127df68bf76a5922f351b22f2f676216dfdf0e38
MahtabSinghAujla <125728731+MahtabSinghAujla@users.noreply.github.com>
2026-05-16 21:02:31 +1000

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,10 +1,17 @@
-#include <Wire.h>
+#include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
 LiquidCrystal_I2C lcd0(0x27,16,2);
 LiquidCrystal_I2C lcd1(0x3F,16,2);
+const int bzr = 3;
+const int p1Y = 13;
+const int p1N = 12;
 
 void setup() {
+  pinMode(bzr, OUTPUT);
+  pinMode(p1Y, INPUT);
+  pinMode(p1N, INPUT);
+
   lcd0.init();
   lcd0.backlight();
   lcd1.init();
@@ -14,10 +21,13 @@ void setup() {
 void loop() {
   lcd0.setCursor(0,0);
   lcd0.print("This is");
-  lcd0.setCursor(0,1);
-  lcd0.print("The Dealer");
+  lcd0.setCursor(0, 1);
+  lcd0.print("Dealer");
 
   lcd1.setCursor(0,0);
   lcd1.print("This is");
-  lcd1.setCursor(0,1);
-  lcd1.print("The Player");
+  lcd1.setCursor(0, 1);
+  lcd1.print("Player 1");
+
+  tone(bzr, 1000);
+}
